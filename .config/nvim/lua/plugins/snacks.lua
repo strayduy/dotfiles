4bb5e09340caee88030b8bae6963a1b535a7b11a
Duy Tran <duy@seesaw.me>
2025-07-23 10:59:32 -0700

    Add snacks.nvim, which replaces indent-blankline and lazygit plugins

---
+++ .config/nvim/lua/plugins/snacks.lua
@@ -0,0 +1,29 @@
+return {
+    "folke/snacks.nvim",
+    priority = 1000,
+    lazy = false,
+    ---@type snacks.Config
+    opts = {
+        dashboard = { example = "advanced" },
+        indent = { char = "┊", scope = { char = "┊" } },
+        lazygit = { enabled = true },
+        notifier = { enabled = true },
+        terminal = { enabled = true },
+    },
+    keys = {
+        {
+            "<Leader>lg",
+            function()
+                Snacks.lazygit()
+            end,
+            desc = "Open [L]azy[G]it",
+        },
+        {
+            "<Leader>t",
+            function()
+                Snacks.terminal()
+            end,
+            desc = "Open [T]erminal",
+        },
+    },
+}
