601fd5d30dd8f52cb73fe9bcb082abc33cbc9b5b
Duy Tran <duy@seesaw.me>
2026-03-20 15:10:32 -0700

    Remove nvim-cmp dependency from autopairs

--- .config/nvim/lua/plugins/autopairs.lua
+++ .config/nvim/lua/plugins/autopairs.lua
@@ -1,14 +1,9 @@
 return {
     "windwp/nvim-autopairs",
     event = { "InsertEnter" },
-    dependencies = {
-        "hrsh7th/nvim-cmp",
-    },
     config = function()
-        -- import nvim-autopairs
         local autopairs = require("nvim-autopairs")
 
-        -- configure autopairs
         autopairs.setup({
             check_ts = true, -- enable treesitter
             ts_config = {
@@ -16,14 +11,5 @@ return {
                 javascript = { "template_string" }, -- don't add pairs in javscript template_string treesitter nodes
             },
         })
-
-        -- import nvim-autopairs completion functionality
-        local cmp_autopairs = require("nvim-autopairs.completion.cmp")
-
-        -- import nvim-cmp plugin (completions plugin)
-        local cmp = require("cmp")
-
-        -- make autopairs and completion work together
-        cmp.event:on("confirm_done", cmp_autopairs.on_confirm_done())
     end,
 }
